9ef47503a975b0ab5bf2dcdb109c6ed61d0eccaa
José Nuñez Morocho <darksoul.js17@gmail.com>
2025-05-09 01:46:17 -0500

    first commit

---
+++ src/main/java/com/joseloc/programacion/reactiva/curso_programacion_reactiva/CursoProgramacionReactivaApplication.java
@@ -0,0 +1,13 @@
+package com.joseloc.programacion.reactiva.curso_programacion_reactiva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoProgramacionReactivaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoProgramacionReactivaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joseloc/programacion/reactiva/curso_programacion_reactiva/CursoProgramacionReactivaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joseloc.programacion.reactiva.curso_programacion_reactiva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoProgramacionReactivaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
